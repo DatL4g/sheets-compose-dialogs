fb7d802022ac1259281f67c917b1f32fab381dd8
Maicol Battistini <maicolbattistini@live.it>
2024-04-16 10:52:02 +0200

    refactor: Moved dependency management from buildSrc to Version Catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import com.android.build.gradle.LibraryExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
